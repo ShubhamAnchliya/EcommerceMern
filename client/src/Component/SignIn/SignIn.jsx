530f3ebcf5d93a23a12fc8274c3f6505feb906ec
Shubham Anchliya <info.shubhamanchliya@gmail.com>
2022-06-27 19:35:19 +0530

    msg27

--- client/src/Component/SignIn/SignIn.jsx
+++ client/src/Component/SignIn/SignIn.jsx
@@ -1,3 +1,110 @@
+// import React, { useState } from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+// import "./SignIn.css";
+
+// const SignIn = () => {
+
+//   // const navigate = useNavigate();
+
+//   const [formData, setFormData] = useState({
+//     email:"",
+//     password:""
+//   })
+
+
+//   const InputEvent = (event) => {
+
+//     const { name, value } = event.target;
+
+//     setFormData((preVal) => {
+
+//       return {
+//         ...preVal,
+//         [name]: value,
+//       };
+
+//     });
+
+//   };
+
+
+//   const formSubmit = (e) => {
+
+//     e.preventDefault();
+//     console.log(formData);
+    
+
+//   }
+
+
+
+//   return (
+
+//     <>
+
+// <div className='logInPage'>
+
+//   <div className="container" id="container">
+//     <div className="form-container sign-in-container">
+//       <form className='formL'  onSubmit={formSubmit} >
+//         <h1>Sign in</h1>
+        
+//         <span className='formLSpan'>or use your account</span>
+//         <input 
+//           type="email" 
+//           name='email'
+//           placeholder="Email" 
+//           value={formData.email}
+//           onChange={InputEvent}
+//           required
+
+//         />
+//         <input 
+//           type="password" 
+//           name='password'
+//           placeholder="Password" 
+//           value={formData.password}
+//           onChange={InputEvent}
+//           required
+//         />
+
+
+//         <button className='buttonL' type='submit'>Sign In</button>
+
+//       </form>
+//     </div>
+
+//     <div className="overlay-container">
+//       <div className="overlay">
+//         <div className="overlay-panel overlay-right">
+//           <h1>Hello, Friend!</h1>
+//           <p>Enter your personal details and start journey with us</p>
+//           <Link to={'/signup'} ><button className="buttonL ghost" id="signUp">Sign Up</button></Link>
+//         </div>
+//       </div>
+//     </div>
+    
+//   </div>
+
+   
+          
+// </div>
+
+//     </>
+//   )
+// }
+
+// export default SignIn;
+
+
+
+
+
+
+
+
+// 27 useHookForm
+
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import "./SignIn.css";
--- client/src/Component/SignUp/SignUp.jsx
+++ client/src/Component/SignUp/SignUp.jsx
@@ -124,137 +124,199 @@
 
 
 
-//24
+//24 use hook form
 
 
 import React,{ useState } from 'react';
 import "./SignUp.css";
 import { Link, useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 const SignUp = () => {
 
   const navigate = useNavigate();
 
-  const [formData, setFormData] = useState({
-     
-      fname:"",
-      email:"",
-      password:""
+  const {  register, handleSubmit, getValues, watch, formState:{errors} } = useForm();
 
-  });
+//   const formData = data => console.log(data);
+
+//   console.log(watch("fName"));
 
 
-  const [errors, setErrors] = useState({});
 
-
-   const validate  = (event, name, value) => {
-
-    switch (name) {
-        case 'fname':
-            
-            break;
+const signupData = (data) => {
     
-        default:
-            break;
-    }
+    console.log(data);
+
+}
+//   const [formData, setFormData] = useState({
+     
+//       fname:"",
+//       email:"",
+//       password:"",
+//       confirmPassword:""
 
 
-   }
+//   });
 
 
-  const InputEvent = (event) => {
+//   const [errors, setErrors] = useState({});
 
-    const { name, value } = event.target;
 
-    validate(event, name, value);
+//    const validate  = (event, name, value) => {
 
-    setFormData ((preVal) => {
-        return {
-            ...preVal,
-            [name]: value
-        };
+//     switch (name) {
+//         case 'fname':
+            
+//             break;
+    
+//         default:
+//             break;
+//     }
+
+
+//    }
+
+
+//   const InputEvent = (event) => {
+
+//     const { name, value } = event.target;
+
+//     // validate(event, name, value);
+
+//     setFormData ((preVal) => {
+//         return {
+//             ...preVal,
+//             [name]: value
+//         };
       
-    });
+//     });
 
-};
+// };
 
 
-  const formSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
+//     handleSubmit = (e) => {
+//         e.preventDefault();
+//         console.log(formData);
+//         navigate("/signin");
 
-  
-
-   
-    navigate("/signin");
-
-  }
+//   }
 
   return (
 
     <>
 
-      <div className='signupPage  blue-gradient '>
+        <div className='signupPage  blue-gradient '>
+    
+            <div className="containers right-panel-active" id="container">
 
-  
+                <div className="form-containers sign-up-containers">
 
-      <div className="containers right-panel-active" id="container">
-          <div className="form-containers sign-up-containers">
+                    <form className='formS' onSubmit={handleSubmit(signupData)}>
 
-              <form className='formS' onSubmit={formSubmit}>
+                        <h1>Create Account</h1>
+                        
+                        <span className='formsSpan' >or use your email for registration</span>
 
-                  <h1>Create Account</h1>
-                  
-                  <span className='formsSpan' >or use your email for registration</span>
+                        <input 
+                            type="text" 
+                            {...register("name", {required: true, minLength:3, maxLength: 20})}
+                            name='name'
+                            placeholder="Name" 
+                            // value={formData.fname}
+                            // onChange={InputEvent}
+                            // required
+                        />
+                        {errors.name?.type=== "required" && <p>Name is required</p> }
+                        {errors.name?.type=== "minLength" && <p>Name should have minimum 3 character</p>  }
+                        {errors.name?.type=== "maxLength" && <p>Name should have maximum 20 character</p> }
 
-                  <input 
-                      type="text" 
-                      name='fname'
-                      placeholder="Name" 
-                      value={formData.fname}
-                      onChange={InputEvent}
-                      required
-                  />
 
 
-                  <input 
-                      type="email" 
-                      name='email'
-                      placeholder="Email" 
-                      value={formData.email}
-                      onChange={InputEvent}
-                      required
-                  />
+                        {/* message: "Entered value does not match email format" */}
 
-                  <input 
-                      type="password" 
-                      name='password'
-                      placeholder="Password" 
-                      value={formData.password}
-                      onChange={InputEvent}
-                      required
-                  />
+                        <input 
+                            type="email" 
+                            // {...register("email", {required:true,pattern: {value: /\S+@\S+\.\S+/}  })}
+                            {...register("email", {required:true,pattern: {value: /\S+@\S+\.\S+/}  })}
+                            name='email'
+                            placeholder="Email" 
+                            // value={formData.email}
+                            // onChange={InputEvent}
+                            // required
+                        />
+                          {errors.email?.type=== "required" && <p>Email is required</p> }
+                          {errors.email?.type=== "pattern" && <p>Invalid email format</p> }
 
-                  <button className='buttons' type='submit'>Sign Up</button>
+                        <input 
+                            type="password" 
+                            {...register("password", {required:true ,minLength:3, })}
+                            name='password'
+                            placeholder="Password" 
+                            // value={formData.password}
+                            // onChange={InputEvent}
+                            // required
+                        />
+                          {errors.password?.type=== "required" && <p>Password is required</p> }
+                          {errors.password?.type=== "minLength" && <p>Password sholud have minimun 3 characters</p> }
+                          
+{/* 
+                          validate: (val: string) => {
+    if (watch('password') != val) {
+      return "Your passwords do no match";
+    }
+  }, */}
 
-              </form>
 
-          </div>
 
-          <div className="overlay-containers">
-              <div className="overlays">
-                  <div className="overlay-panels overlay-lefts">
-                      <h1>Welcome Back!</h1>
-                      <p>To keep connected with us please login with your personal info</p>
-                      <Link to={'/signin'}><button className="buttons ghost" id="signIn">Sign In</button></Link>
-                  </div>
-                  
-              </div>
-          </div>
-      </div>
-      </div>
+                        <input 
+                            type="password" 
+                            {...register("confirmPassword", 
+                                {
+                                    required:true ,
+                                    validate: (value) => {
+                                        const { password } = getValues();
+                                        return password === value || "werre";
+                                    }
+
+                                }
+                            )}
+                            name='confirmPassword'
+                            placeholder="Confirm Password" 
+                            // value={formData.confirmPassword}
+                            // onChange={InputEvent}
+                            // required
+                        />
+                          {errors.confirmPassword?.type=== "required" && <p>Confirm Password is required</p> }
+                          {errors.confirmPassword?.type=== "ers" && <p>Passwords should match!</p> }
+
+
+
+                        <button className='buttons' type='submit'>Sign Up</button>
+
+                    </form>
+
+                </div>
+
+                <div className="overlay-containers">
+
+                    <div className="overlays">
+                        <div className="overlay-panels overlay-lefts">
+                            <h1>Welcome Back!</h1>
+                            <p>To keep connected with us please login with your personal info</p>
+                            <Link to={'/signin'}><button className="buttons ghost" id="signIn">Sign In</button></Link>
+                        </div>
+                        
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
      
     </>
+
   )
 }
 
